b59a15475908b4df1e9358450f5a5359090d28ba
Bigg-Kayy <bryta.fosu@gmail.com>
2026-05-31 05:18:52 -0700

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,28 @@
+# Name: FOSU BRIGHT ABBAN
+# STUDENT ID: 2425404196
+# Email: 2425404196@gctu.edu.gh
+# Grader: BRIGHT
+# On my honor, BRIGHT FOSU ABBAN, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
 def main():
-  pass #code goes here
+    #Print my initials
+
+ print()
+ print("...BFA")
+ print()
+
+ print("...BBBBBBBBB...........FFFFFFFFFFFF..........AAAAAAAA..")
+ print("...BBBBBBBBBBB.........FFFFFFFFFFFF........AAAAAAAAAAAA")
+ print("...BB........BB........FF..................AA........AA")
+ print("...BB........BB........FF..................AA........AA")
+ print("...BB........BB........FF..................AA........AA")
+ print("...BBBBBBBBBBBB........FFFFFFFFFFFF........AAAAAAAAAAAA")
+ print("...BB........BB........FF..................AA........AA")
+ print("...BB........BB........FF..................AA........AA")
+ print("...BB........BB...**...FF.............**...AA........AA")
+ print("...BBBBBBBBBBBB...**...FF.............**...AA........AA")
 
 
-
+ print()
 main()
